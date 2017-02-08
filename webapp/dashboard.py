cf6b045ebc0fd43bb8adfc2606f9130774a2366f
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-02-08 22:59:00 +0000

    Added a task workflow line, tweaked a bit.

--- webapp/dashboard.py
+++ webapp/dashboard.py
@@ -54,7 +54,7 @@ def overview():
 def load_service_status():
     services = None
     # Attempt to load current statistics, backtracking until stats are available:
-    for mins in range(0,100):
+    for mins in range(0,500):
         try:
             status_date = datetime.datetime.today() - datetime.timedelta(minutes=mins)
             json_file = CheckStatus(date=status_date).output().path
@@ -62,11 +62,13 @@ def load_service_status():
             services = load_services(json_file)
             services['status_date'] = status_date.isoformat()
             services['status_date_delta'] = mins
-            if( mins > 1 ):
-                status['status_date_warning'] = "Status data is %s minutes old!" % mins
-            break
+            if( mins > 2 ):
+                services['status_date_warning'] = "Status data is %s minutes old!" % mins
         except Exception as e:
-            app.logger.info("Could not load %i mins ago..." % mins)
+            app.logger.info("Could not load %i mins ago... %s" % (mins, e))
+
+        if services is not None:
+            break
 
     if services is None:
         abort(500, "Could not load a recent service status file!")
