07a79c7286df773f8967dc33334cee60c95e9cf8
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-07-02 14:40:13 +0100

    Fix-up DFS Health page scraper.

--- hdfs_exporter/webapp.py
+++ hdfs_exporter/webapp.py
@@ -4,10 +4,12 @@ import time
 import socket
 import urllib2
 import logging
-from multiprocessing import Pool, Process
+import requests
+import lxml.etree as etree
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
@@ -18,78 +20,54 @@ socket.setdefaulttimeout(TIMEOUT)
 
 class HDFSCollector(object):
 
-    def load_as_json(self, filename):
-        script_dir = os.path.dirname(__file__)
-        file_path = os.path.join(script_dir, filename)
-        with open(file_path, 'r') as fi:
-            return json.load(fi)
-
-    def run_api_requests(self):
-        services = self.load_as_json(os.path.join(os.path.dirname(__file__), 'hdfs-services.json'))
-        pool = Pool(20)
-
-        # Parallel check for H3 job status:
-        argsv = []
-        for hdfs in services.get('hdfs', []):
-            argsv.append((hdfs, services['hdfs'][hdfs]['url']))
-        # Wait for all...
-        results = pool.map(get_hdfs_status, argsv)
-        for hdfs, state in results:
-            services['hdfs'][hdfs]['state'] = state
-
-        return services
-
     def collect(self):
-        stats = self.run_api_requests()
+        try:
+            stats = self.get_hdfs_status(os.environ.get('HDFS_HEALTH_PAGE'))
+            service_label = os.environ.get("HDFS_SERVICE_LABEL","production")
 
-        metric = GaugeMetricFamily(
-            'jenkins_job_last_successful_build_timestamp_seconds',
-            'Jenkins build timestamp in unixtime for lastSuccessfulBuild',
-            labels=["jobname"])
+            urb = GaugeMetricFamily(
+                'hdfs_under_replicated_block_count',
+                'Total number of under-replicated blocks',
+                labels=['service'])
+            urb.add_metric([service_label], stats.get('under-replicated-blocks', None))
+            yield urb
 
-        result = json.load(urllib2.urlopen(
-            "http://jenkins:8080/api/json?tree="
-            + "jobs[name,lastSuccessfulBuild[timestamp]]"))
+            hup = GaugeMetricFamily(
+                'hdfs_used_percent',
+                'HDFS used space as a percentage',
+                labels=['service'])
+            hup.add_metric([service_label], stats.get('percent-used', None))
+            yield hup
 
-        for job in result['jobs']:
-            name = job['name']
-            # If there's a null result, we want to export a zero.
-            status = job['lastSuccessfulBuild'] or {}
-            metric.add_metric([name], status.get('timestamp', 0) / 1000.0)
+            hnc = GaugeMetricFamily(
+                'hdfs_node_count',
+                'HDFS node counts',
+                labels=['service','status'])
+            hnc.add_metric([service_label, 'live'], stats.get('live-nodes', None))
+            hnc.add_metric([service_label, 'dead'], stats.get('dead-nodes', None))
+            yield hnc
 
-        yield metric
+        except Exception as e:
+            logger.exception("FAILED")
 
-
-def get_hdfs_status(args):
-    hdfs_id, url = args
-    state = {}
-    try:
+    def get_hdfs_status(self, url):
+        state = {}
         logger.info("Getting status for hdfs %s" % (url))
-        r = s.get(url, timeout=(TIMEOUT, TIMEOUT))
+        r = requests.get(url, timeout=(TIMEOUT, TIMEOUT))
         state['status'] = "%s" % r.status_code
         if r.status_code / 100 == 2:
-            state['status-class'] = "status-good"
             tree = etree.fromstring(r.text, etree.HTMLParser())
             percent = tree.xpath("//div[@id='dfstable']//tr[5]/td[3]")[0].text
             percent = percent.replace(" ", "")
-            state['percent'] = percent
             state['percent-used'] = float(percent.replace("%",""))
             state['remaining'] = tree.xpath("//div[@id='dfstable']//tr[4]/td[3]")[0].text.replace(" ", "")
             underr = int(tree.xpath("//div[@id='dfstable']//tr[10]/td[3]")[0].text)
             state['under-replicated-blocks'] = underr
             state['live-nodes'] = int(tree.xpath("//div[@id='dfstable']//tr[7]/td[3]")[0].text)
             state['dead-nodes'] = int(tree.xpath("//div[@id='dfstable']//tr[8]/td[3]")[0].text)
-            if underr != 0:
-                state['status'] = "HDFS has %i under-replicated blocks!" % underr
-                state['status-class'] = "status-warning"
-        else:
-            state['status-class'] = "status-warning"
-    except Exception as e:
-        logger.exception(e)
-        state['status'] = "DOWN"
-        state['status-class'] = "status-alert"
 
-    return hdfs_id, state
+        return state
+
 
 if __name__ == "__main__":
     REGISTRY.register(HDFSCollector())
