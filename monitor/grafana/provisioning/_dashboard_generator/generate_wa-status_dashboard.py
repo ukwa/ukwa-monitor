260749280a13860c44e37950827f7e5da8a914ee
gil <gil.hoggarth@bl.uk>
2019-01-21 15:43:05 +0000

    adding blackbox container and http service monitoring

--- monitor/grafana/provisioning/_dashboard_generator/generate_wa-status_dashboard.py
+++ monitor/grafana/provisioning/_dashboard_generator/generate_wa-status_dashboard.py
@@ -12,8 +12,8 @@ import datetime
 outFile = 'wa-status_json.out'
 
 # dashboard prefixes
-dashTitle = 'WA Status-'
-dashUid = 'wast-'
+dashTitle = 'WA Status'
+dashUid = 'wast'
 
 # template files
 header = 'templates/header'
@@ -79,26 +79,32 @@ def replace_output_single(outHandle, **kwargs):
 
 	# add expr
 	if kwargs['title'] == 'Up':
-		exprUp = 'sum(1 - up{job=\\"' + kwargs['job'] + '\\"})'
-		templateCode = templateCode.replace('<expr>', exprUp)
+		expr = 'sum(1 - up{job=\\"' + kwargs['job'] + '\\"})'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'CPU':
-		exprCpu = 'count(sum(irate(node_cpu_seconds_total{job=\\"' + kwargs['job'] + '\\",mode=\\"idle\\"}[5m]) < 0.1) by (instance)) OR vector(0)'
-		templateCode = templateCode.replace('<expr>', exprCpu)
+		expr = 'count(sum(irate(node_cpu_seconds_total{job=\\"' + kwargs['job'] + '\\",mode=\\"idle\\"}[5m]) < 0.1) by (instance)) OR vector(0)'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'Dsk':
-		exprDsk = 'count((node_filesystem_avail_bytes{job=\\"' + kwargs['job'] + '\\",fstype!~\\"tmpfs|cifs\\"} / node_filesystem_size_bytes{job=\\"' + kwargs['job'] + '\\",fstype!~\\"tmpfs|cifs\\"}) < 0.04) OR vector(0)'
-		templateCode = templateCode.replace('<expr>', exprDsk)
+		expr = 'count((node_filesystem_avail_bytes{job=\\"' + kwargs['job'] + '\\",fstype!~\\"tmpfs|cifs\\"} / node_filesystem_size_bytes{job=\\"' + kwargs['job'] + '\\",fstype!~\\"tmpfs|cifs\\"}) < 0.04) OR vector(0)'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'Mem':
-		exprMem = 'count((node_memory_MemFree_bytes{job=\\"' + kwargs['job'] + '\\"} / node_memory_MemTotal_bytes{job=\\"' + kwargs['job'] + '\\"}) < 0.01) OR vector(0)'
-		templateCode = templateCode.replace('<expr>', exprMem)
+		expr = 'count((node_memory_MemFree_bytes{job=\\"' + kwargs['job'] + '\\"} / node_memory_MemTotal_bytes{job=\\"' + kwargs['job'] + '\\"}) < 0.01) OR vector(0)'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'Nodes':
-		exprMem = 'hdfs_node_count{status=\\"dead\\",instance=\\"ingest:9118\\"}'
-		templateCode = templateCode.replace('<expr>', exprMem)
+		expr = 'hdfs_node_count{status=\\"dead\\",instance=\\"ingest:9118\\"}'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'Under-rep':
-		exprMem = 'hdfs_under_replicated_block_count{instance=\\"ingest:9118\\"}'
-		templateCode = templateCode.replace('<expr>', exprMem)
+		expr = 'hdfs_under_replicated_block_count{instance=\\"ingest:9118\\"}'
+		templateCode = templateCode.replace('<expr>', expr)
 	elif kwargs['title'] == 'Used':
-		exprMem = 'hdfs_used_percent{instance=\\"ingest:9118\\"}'
-		templateCode = templateCode.replace('<expr>', exprMem)
+		expr = 'hdfs_used_percent{instance=\\"ingest:9118\\"}'
+		templateCode = templateCode.replace('<expr>', expr)
+	elif kwargs['title'] == 'UTR':
+		expr = 'count(uptimerobot_monitor_up==0) by (name) OR vector(0)'
+		templateCode = templateCode.replace('<expr>', expr)
+	elif kwargs['title'] == 'da-WWW':
+		expr = 'count(probe_http_status_code{job=\\"' + kwargs['job'] + '\\"} != 200) OR vector(0)'
+		templateCode = templateCode.replace('<expr>', expr)
 
 	# remove last comma if last panel
 	if 'lastPanel' in kwargs:
@@ -141,16 +147,18 @@ def main():
 	replace_output_single(outHandle, tmpFl=panelSingle, job='discovery_access', title='CPU', h=1, w=2, x=18, y=1)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='discovery_access', title='Dsk', h=1, w=2, x=20, y=1)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='discovery_access', title='Mem', h=1, w=2, x=22, y=1)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='discovery_access', title='UTR', h=1, w=2, x=16, y=2)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='da-access-http', title='da-WWW', h=1, w=2, x=18, y=2)
 	replace_output_title(outHandle, tmpFl=panelTitle, job='gluster', title='Gluster', h=1, w=8, x=8, y=4)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='gluster', title='Up', h=1, w=2, x=8, y=5)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='gluster', title='CPU', h=1, w=2, x=10, y=5)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='gluster', title='Dsk', h=1, w=2, x=12, y=5)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='gluster', title='Mem', h=1, w=2, x=14, y=5)
-	replace_output_title(outHandle, tmpFl=panelTitle, job='solr', title='Solr', h=1, w=8, x=16, y=2)
-	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Up', h=1, w=2, x=16, y=3)
-	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='CPU', h=1, w=2, x=18, y=3)
-	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Dsk', h=1, w=2, x=20, y=3)
-	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Mem', h=1, w=2, x=22, y=3, thresholds='0.1,4.1', colour='#ba43a9')
+	replace_output_title(outHandle, tmpFl=panelTitle, job='solr', title='Solr', h=1, w=8, x=16, y=3)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Up', h=1, w=2, x=16, y=4)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='CPU', h=1, w=2, x=18, y=4)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Dsk', h=1, w=2, x=20, y=4)
+	replace_output_single(outHandle, tmpFl=panelSingle, job='solr', title='Mem', h=1, w=2, x=22, y=4, thresholds='0.1,4.1', colour='#ba43a9')
 	replace_output_title(outHandle, tmpFl=panelTitle, job='infrastructure', title='Infrastructure', h=1, w=8, x=8, y=6)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='infrastructure', title='Up', h=1, w=2, x=8, y=7)
 	replace_output_single(outHandle, tmpFl=panelSingle, job='infrastructure', title='CPU', h=1, w=2, x=10, y=7)
@@ -164,7 +172,8 @@ def main():
 	output(outHandle, tmpFl = panelFooter)
 	date = datetime.datetime.now()
 	formattedDate = date.strftime("%Y%m%d%H%M%S")
-	replace_output_footer(outHandle, tmpFl = footer, title = dashTitle + formattedDate, uid = dashUid + formattedDate)
+#	replace_output_footer(outHandle, tmpFl = footer, title = dashTitle +'-'+ formattedDate, uid = dashUid +'-'+ formattedDate)
+	replace_output_footer(outHandle, tmpFl = footer, title = dashTitle, uid = dashUid)
 
 	outHandle.close()
 
--- monitor/start_prometheus.sh
+++ monitor/start_prometheus.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-source ~/gitlab/ukwa-monitor/prometheus.sh
+source ~/gitlab/ukwa-monitor/monitoring.sh
 docker stack deploy -c docker-compose.yml prometheus
