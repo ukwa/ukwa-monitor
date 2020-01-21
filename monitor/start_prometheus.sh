60dcb25c19f636437414781a1eed080d7efb0186
root <root@dev1.n45.wa.bl.uk>
2020-01-21 18:00:11 +0000

    Envaring values for dev,beta,prod deployment

--- monitor/start_prometheus.sh
+++ monitor/start_prometheus.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+export DATA_GRAFANA=/mnt/nfs/data/ukwa-monitor/grafana
+export DATA_PROMETHEUS=/mnt/nfs/data/ukwa-monitor/prometheus
+export DATA_ALERTMANAGER=/mnt/nfs/data/ukwa-monitor/altermanager
+export HTTP_PROXY=http://explorer2:3128/
+
 source ~/gitlab/ukwa-monitor/monitoring.sh
 docker stack deploy -c docker-compose.yml prometheus
