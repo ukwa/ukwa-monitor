92e2e982e572920bc30c1e2c8097d4c224f7d1cf
gil <gil.hoggarth@bl.uk>
2020-01-22 10:41:52 +0000

    working on prod

--- monitor/prod/start_prometheus.sh
+++ monitor/prod/start_prometheus.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 export VISUALIZER_PORT=8080
+export GRAFANA_PORT=3000
+export PROMETHEUS_PORT=9090
+export ALERTMANAGER_PORT=9093
 export DATA_GRAFANA=/opt/data/grafana
 export DATA_PROMETHEUS=/opt/data/prometheus
-export DATA_ALERTMANAGER=/opt/data/altermanager
+export DATA_ALERTMANAGER=/opt/data/alertmanager
 export HTTP_PROXY=http://explorer2:3128/
 
 source ~/gitlab/ukwa-monitor/monitoring.sh
