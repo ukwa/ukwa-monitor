7cbd625088d06b11f1b878cb2acb1e714b4952f0
gil <gil.hoggarth@bl.uk>
2020-02-26 16:25:18 +0000

    Finally back to the real production service

--- monitor/prod/start_monitor.sh
+++ monitor/prod/start_monitor.sh
@@ -8,9 +8,9 @@ export VISUALIZER_PORT=8080
 export GRAFANA_PORT=3000
 export PROMETHEUS_PORT=9090
 export ALERTMANAGER_PORT=9093
-export DATA_GRAFANA=/mnt/gluster/prod/data/monitor/grafana
-export DATA_PROMETHEUS=/mnt/gluster/prod/data/monitor/prometheus
-export DATA_ALERTMANAGER=/mnt/gluster/prod/data/monitor/alertmanager
+export DATA_GRAFANA=/opt/data/grafana
+export DATA_PROMETHEUS=/opt/data/prometheus
+export DATA_ALERTMANAGER=/opt/data/alertmanager
 export HTTP_PROXY=http://explorer2:3128/
 
 export ALERT_EMAIL='wa-sysadm@bl.uk'
