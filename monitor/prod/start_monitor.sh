aa732ad15ebff6567c1afa2c4c218b4ca78bc94d
root <root@prod1.n45.wa.bl.uk>
2020-02-26 16:04:59 +0000

    Discarding unnecessary config

--- monitor/prod/start_monitor.sh
+++ monitor/prod/start_monitor.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-export HOST_IP=$(hostname -i |  grep -oP "192\.168\.45\.[0-9]+")
 export PROMETHEUS_SERVICE_NAME='monitor-prometheus.api.wa.bl.uk'
 export ALERTMANAGER_SERVICE_NAME='monitor-alerts.api.wa.bl.uk'
 export GRAFANA_SERVICE_NAME='monitor-grafana.api.wa.bl.uk'
@@ -9,9 +8,9 @@ export VISUALIZER_PORT=8080
 export GRAFANA_PORT=3000
 export PROMETHEUS_PORT=9090
 export ALERTMANAGER_PORT=9093
-export DATA_GRAFANA=/mnt/gluster/prod/data/grafana
-export DATA_PROMETHEUS=/mnt/gluster/prod/data/prometheus
-export DATA_ALERTMANAGER=/mnt/gluster/prod/data/alertmanager
+export DATA_GRAFANA=/mnt/gluster/prod/data/monitor/grafana
+export DATA_PROMETHEUS=/mnt/gluster/prod/data/monitor/prometheus
+export DATA_ALERTMANAGER=/mnt/gluster/prod/data/monitor/alertmanager
 export HTTP_PROXY=http://explorer2:3128/
 
 export ALERT_EMAIL='wa-sysadm@bl.uk'
